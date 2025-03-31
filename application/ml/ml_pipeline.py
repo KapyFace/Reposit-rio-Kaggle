3436e1d990e603594e3dbd4534332211da73311d
Raffael Guideti Miello <raffaelguideti@gmail.com>
2025-03-31 09:11:49 -0300

    Ajuste de código
    Ajuste de código para funcionar normalmente

--- application/ml/ml_pipeline.py
+++ application/ml/ml_pipeline.py
@@ -4,7 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from infrastructure.logger import get_logger
+from infrastructure.logger.logger import get_logger
+
 
 logger = get_logger(__name__)
 
